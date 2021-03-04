62909bd6635ed78735f745c0a3e664373c7223c8
atsushi.arayashiki <atsushi.arayashiki415@gmail.com>
2021-03-05 06:06:24 +0900

    test

---
+++ hello/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   <h1>名前は{{number.name}}</h1>
+   <h1>年齢は{{number.age}}</h1>
+   <h1>生まれは{{number.from}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      number:{
+        name: "荒屋敷惠美子",
+        age: 70,
+        from: "是川"
+      }
+    };
+  }
+};
+</script>
+
---
+++ js/main.js
@@ -0,0 +1,3 @@
+<Templet>
+  
+</Templet>
